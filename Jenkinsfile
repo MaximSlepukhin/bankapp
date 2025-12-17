5b6bd98ba2985a1adb56dcabfab1eacc64ac8871
Maxim <slepuhinmaksym@yandex.ru>
2025-12-17 21:48:29 +0300

    Jenkinsfile final

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,12 +10,14 @@ pipeline {
 
         stage('Check Versions') {
             steps {
+                // Здесь указаны полные пути к инструментам, чтобы Jenkins их нашёл
                 sh '''
                     echo "Checking tool versions..."
                     /usr/local/bin/docker --version
                     /opt/homebrew/bin/helm version --short
                     /usr/local/bin/kubectl version --client
                     /Users/maksim/apps/apache-maven-3.9.9/bin/mvn -v
+                    /opt/homebrew/bin/minikube version
                 '''
             }
         }
@@ -23,7 +25,6 @@ pipeline {
         stage('Build With Maven') {
             steps {
                 script {
-                    // Список микросервисов
                     def services = [
                         "accounts-service",
                         "blocker-service",
@@ -32,15 +33,91 @@ pipeline {
                         "exchange-service",
                         "front-ui",
                         "notifications-service",
-                        "transfer-service"
+                        "transfer-service",
                     ]
 
                     services.each { service ->
                         sh """
                             echo "Building ${service}..."
                             cd ${service}
-                            /Users/maksim/apps/apache-maven-3.9.9/bin/mvn clean package -DskipTests
+                            /Users/maksim/apps/apache-maven-3.9.9/bin/mvn clean package
                             cd ..
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Build Docker Images') {
+            steps {
+                script {
+                    // Добавляем пути к Docker и Minikube в PATH
+                    env.PATH = "/usr/local/bin:/opt/homebrew/bin:${env.PATH}"
+
+                    def services = [
+                        "accounts-service",
+                        "blocker-service",
+                        "cash-service",
+                        "exchange-generator-service",
+                        "exchange-service",
+                        "front-ui",
+                        "notifications-service",
+                        "transfer-service",
+                    ]
+
+                    // Настройка Minikube Docker окружения
+                    sh '''
+                        echo "Setting up Minikube Docker environment..."
+                        eval $("${env.PATH.split(':')[1]}/minikube" docker-env)
+                    '''
+
+                    services.each { service ->
+                        sh """
+                            echo "Building Docker image for ${service}..."
+                            /usr/local/bin/docker build -t ${service}:latest -f ${service}/Dockerfile ${service}
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Deploy Databases with Helm') {
+            steps {
+                script {
+                    def dbs = [
+                        "accounts-db",
+                        "keycloak-db"
+                    ]
+
+                    dbs.each { db ->
+                        sh """
+                            echo "Deploying ${db}..."
+                            /opt/homebrew/bin/helm upgrade --install ${db} ./helm/bankapp/charts/${db} --namespace dev --create-namespace --wait --timeout 300s
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Deploy Microservices with Helm') {
+            steps {
+                script {
+                    def services = [
+                        "accounts-service",
+                        "blocker-service",
+                        "cash-service",
+                        "exchange-generator-service",
+                        "exchange-service",
+                        "front-ui",
+                        "notifications-service",
+                        "transfer-service",
+                        "keycloak"
+                    ]
+
+                    services.each { service ->
+                        sh """
+                            echo "Deploying ${service}..."
+                            /opt/homebrew/bin/helm upgrade --install ${service} ./helm/bankapp/charts/${service} --namespace dev --wait --timeout 300s
                         """
                     }
                 }
--- accounts-service/src/test/java/controller/AccountControllerTest.java
+++ accounts-service/src/test/java/controller/AccountControllerTest.java
@@ -1,95 +1,95 @@
-//package controller;
-//
-//import com.github.maximslepukhin.AccountsServiceApplication;
-//import com.github.maximslepukhin.controller.AccountController;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//
-//import com.github.maximslepukhin.service.AccountService;
-//import org.mockito.Mockito;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//
-//import java.math.BigDecimal;
-//import java.util.List;
-//
-//import static org.mockito.Mockito.doThrow;
-//
-//
-//@WebMvcTest(AccountController.class)
-//@ContextConfiguration(classes = AccountsServiceApplication.class)
-//@AutoConfigureMockMvc(addFilters = false)
-//class AccountControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private AccountService accountService;
-//
-//    @Test
-//    void getBalance_ShouldReturnBalance() throws Exception {
-//        Mockito.when(accountService.getBalance("john", "USD"))
-//                .thenReturn(BigDecimal.valueOf(123.45));
-//
-//        mockMvc.perform(get("/api/accounts/john/USD"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().string("123.45"));
-//    }
-//
-//    @Test
-//    void debit_ShouldReturnOk() throws Exception {
-//        mockMvc.perform(post("/api/accounts/john/USD/debit")
-//                        .param("amount", "50"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void debit_ShouldReturnBadRequest_WhenError() throws Exception {
-//        doThrow(new RuntimeException("Ошибка списания"))
-//                .when(accountService).debit("john", "USD", BigDecimal.valueOf(50));
-//
-//        mockMvc.perform(post("/api/accounts/john/USD/debit")
-//                        .param("amount", "50"))
-//                .andExpect(status().isBadRequest())
-//                .andExpect(jsonPath("$.error").value("Ошибка списания"));
-//    }
-//
-//    @Test
-//    void credit_ShouldReturnOk() throws Exception {
-//        mockMvc.perform(post("/api/accounts/john/USD/deposit")
-//                        .param("amount", "100"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void updateBalance_ShouldReturnNewBalance() throws Exception {
-//        Mockito.when(accountService.updateAccountBalance("john", "USD", BigDecimal.valueOf(200)))
-//                .thenReturn(BigDecimal.valueOf(300));
-//
-//        mockMvc.perform(post("/api/accounts/john/USD")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("200"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.balance").value(300));
-//    }
-//
-//    @Test
-//    void getCurrencies_ShouldReturnList() throws Exception {
-//        Mockito.when(accountService.getCurrencies("john"))
-//                .thenReturn(List.of("RUB", "USD"));
-//
-//        mockMvc.perform(get("/api/accounts/john/currencies"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$[0]").value("RUB"))
-//                .andExpect(jsonPath("$[1]").value("USD"));
-//    }
-//}
+package controller;
+
+import com.github.maximslepukhin.AccountsServiceApplication;
+import com.github.maximslepukhin.controller.AccountController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.github.maximslepukhin.service.AccountService;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.mockito.Mockito.doThrow;
+
+
+@WebMvcTest(AccountController.class)
+@ContextConfiguration(classes = AccountsServiceApplication.class)
+@AutoConfigureMockMvc(addFilters = false)
+class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AccountService accountService;
+
+    @Test
+    void getBalance_ShouldReturnBalance() throws Exception {
+        Mockito.when(accountService.getBalance("john", "USD"))
+                .thenReturn(BigDecimal.valueOf(123.45));
+
+        mockMvc.perform(get("/api/accounts/john/USD"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("123.45"));
+    }
+
+    @Test
+    void debit_ShouldReturnOk() throws Exception {
+        mockMvc.perform(post("/api/accounts/john/USD/debit")
+                        .param("amount", "50"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void debit_ShouldReturnBadRequest_WhenError() throws Exception {
+        doThrow(new RuntimeException("Ошибка списания"))
+                .when(accountService).debit("john", "USD", BigDecimal.valueOf(50));
+
+        mockMvc.perform(post("/api/accounts/john/USD/debit")
+                        .param("amount", "50"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("Ошибка списания"));
+    }
+
+    @Test
+    void credit_ShouldReturnOk() throws Exception {
+        mockMvc.perform(post("/api/accounts/john/USD/deposit")
+                        .param("amount", "100"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateBalance_ShouldReturnNewBalance() throws Exception {
+        Mockito.when(accountService.updateAccountBalance("john", "USD", BigDecimal.valueOf(200)))
+                .thenReturn(BigDecimal.valueOf(300));
+
+        mockMvc.perform(post("/api/accounts/john/USD")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("200"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value(300));
+    }
+
+    @Test
+    void getCurrencies_ShouldReturnList() throws Exception {
+        Mockito.when(accountService.getCurrencies("john"))
+                .thenReturn(List.of("RUB", "USD"));
+
+        mockMvc.perform(get("/api/accounts/john/currencies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]").value("RUB"))
+                .andExpect(jsonPath("$[1]").value("USD"));
+    }
+}
