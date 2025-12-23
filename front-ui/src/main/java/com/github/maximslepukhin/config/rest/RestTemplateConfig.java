b29ede8a86e59dfbdd02e37519786bd442bb7aa0
Maxim <slepuhinmaksym@yandex.ru>
2025-12-23 03:09:51 +0300

    Add Jenkinsfile

--- front-ui/src/main/java/com/github/maximslepukhin/config/rest/RestTemplateConfig.java
+++ front-ui/src/main/java/com/github/maximslepukhin/config/rest/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.github.maximslepukhin.config.rest;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,14 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getInterceptors().add((request, body, execution) -> {
-            System.out.println("Запрос: " + request.getMethod() + " " + request.getURI());
-            var response = execution.execute(request, body);
-            System.out.println("Ответ: " + response.getStatusCode());
-            return response;
-        });
-        return restTemplate;
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
